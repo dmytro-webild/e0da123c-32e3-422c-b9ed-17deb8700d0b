4a553ea78d54f0e3658595c8840d5aa8e0af1982
bender <kudin.dima@gmail.com>
2026-07-14 10:36:01 +0000

    Switch to version 17: modified src/components/Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -109,7 +109,7 @@ export default function Layout() {
                 </li>
                 <li className="flex items-start gap-3">
                   <MapPin className="w-5 h-5 text-green-500 shrink-0 mt-0.5" />
-                  <span className="text-accent"><br/>Tashkent, Uzbekistan</span>
+                  <span className="text-accent">Nest One Business Center<br/>Tashkent, Uzbekistan</span>
                 </li>
               </ul>
             </div>
